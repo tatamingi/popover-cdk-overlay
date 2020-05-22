6613ff0abe2ea6cd7df1cab02a34b54f0c1afc3e
tatamingi <tatamingi@gmail.com>
2020-05-22 10:46:00 +0300

    create popover service

---
+++ src/app/services/Positions.ts
@@ -0,0 +1,58 @@
+export const Positions = {
+  'top': {
+    originX : 'center',
+    originY : 'top',
+    overlayX: 'center',
+    overlayY: 'bottom',
+    offsetY: -8
+  },
+  'bottom': {
+    originX : 'center',
+    originY : 'bottom',
+    overlayX: 'center',
+    overlayY: 'top',
+    offsetY: 8
+  },
+  'right': {
+    originX : 'end',
+    originY : 'center',
+    overlayX: 'start',
+    overlayY: 'center',
+    offsetX: 8
+  },
+  'left': {
+    originX : 'start',
+    originY : 'center',
+    overlayX: 'end',
+    overlayY: 'center',
+    offsetX: -8
+  },
+  'top-right': {
+    originX: 'end',
+    originY: 'top',
+    overlayX: 'end',
+    overlayY: 'bottom',
+    offsetY: -8
+  },
+  'top-left': {
+    originX: 'start',
+    originY: 'top',
+    overlayX: 'start',
+    overlayY: 'bottom',
+    offsetY: -8
+  },
+  'bottom-right': {
+    originX: 'end',
+    originY: 'bottom',
+    overlayX: 'end',
+    overlayY: 'top',
+    offsetY: 8
+  },
+  'bottom-left': {
+    originX: 'start',
+    originY: 'bottom',
+    overlayX: 'start',
+    overlayY: 'top',
+    offsetY: 8
+  }
+};
---
+++ src/app/services/popover.service.ts
@@ -0,0 +1,81 @@
+import {ElementRef, Injectable, Injector} from '@angular/core';
+import {ConnectedPosition, Overlay} from '@angular/cdk/overlay';
+import {ComponentPortal, PortalInjector} from '@angular/cdk/portal';
+import {PopoverComponent} from '../components/popover/popover.component';
+import {PopoverContent, PopoverRef} from './popover-ref';
+import {Positions} from './Positions';
+import {PopoverPosition} from '../directives/popover.directive';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopoverService {
+
+  constructor(
+    private overlay: Overlay,
+    private injector: Injector
+  ) {
+  }
+
+  open(origin: ElementRef, content: PopoverContent, position: PopoverPosition): PopoverRef {
+    const positionStrategy = this._getPositionStrategy(origin, position);
+    const overlayRef = this.overlay.create({positionStrategy});
+    const popoverRef = new PopoverRef(overlayRef, content);
+    const injector = this._createInjector(popoverRef, this.injector);
+    const popoverPortal = new ComponentPortal(PopoverComponent, null, injector);
+
+    overlayRef.attach(popoverPortal);
+
+    return popoverRef;
+  }
+
+  private _createInjector(popoverRef: PopoverRef, injector: Injector) {
+    const tokens = new WeakMap([[PopoverRef, popoverRef]]);
+    return new PortalInjector(injector, tokens);
+  }
+
+  private _getPositionStrategy(origin: ElementRef, position: PopoverPosition) {
+    const mainPosition = Positions[position] as ConnectedPosition;
+    const fallbackPosition = this._invertPosition(position);
+
+    return this.overlay.position()
+      .flexibleConnectedTo(origin)
+      // желаемое положение popover в порядке приоритетности на случай, если popover не может быть расположен в заданном положении
+      .withPositions([mainPosition, fallbackPosition]);
+  }
+
+  /** Если при заданном position popover не помещается во viewport, отобразим в противоположном направлении */
+  private _invertPosition(position: string): ConnectedPosition {
+    let fallbackPosition;
+
+    switch (position) {
+      case 'top':
+        fallbackPosition = Positions.bottom;
+        break;
+      case 'bottom':
+        fallbackPosition = Positions.top;
+        break;
+      case 'left':
+        fallbackPosition = Positions.right;
+        break;
+      case 'right':
+        fallbackPosition = Positions.left;
+        break;
+      case 'top-right':
+        fallbackPosition = Positions['bottom-right'];
+        break;
+      case 'top-left':
+        fallbackPosition = Positions['bottom-left'];
+        break;
+      case 'bottom-right':
+        fallbackPosition = Positions['top-right'];
+        break;
+      case 'bottom-left':
+        fallbackPosition = Positions['top-left'];
+        break;
+    }
+
+    return fallbackPosition as ConnectedPosition;
+  }
+}
