6f779db335fe8538527f631dc1dcc93c5320bd03
tatamingi <tatamingi@gmail.com>
2020-05-22 10:48:32 +0300

    popover in use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'popover';
+  fallbackPositionWarning = 'Этот текст не помещается во viewport, поэтому автоматически отображается в противоположном направлении';
+  fallbackPositionDescription = 'Если текст не помещается по viewport, он автоматически отобразится в противоположнм направлении';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 import { AppComponent } from './app.component';
+import {PopoverService} from './services/popover.service';
+import { PopoverComponent } from './components/popover/popover.component';
+import { PopoverDirective } from './directives/popover.directive';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopoverComponent,
+    PopoverDirective
   ],
+  entryComponents: [PopoverComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OverlayModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [PopoverService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
