01d1145b8ae798f6d146c6db67c99d2d918f4a4d
tatamingi <tatamingi@gmail.com>
2020-06-30 13:33:22 +0300

    add popover arrow

--- src/app/services/Positions.ts
+++ src/app/services/Positions.ts
@@ -4,55 +4,63 @@ export const Positions = {
     originY : 'top',
     overlayX: 'center',
     overlayY: 'bottom',
-    offsetY: -8
+    offsetY: -8,
+    panelClass: ['top']
   },
   'bottom': {
     originX : 'center',
     originY : 'bottom',
     overlayX: 'center',
     overlayY: 'top',
-    offsetY: 8
+    offsetY: 8,
+    panelClass: ['bottom']
   },
   'right': {
     originX : 'end',
     originY : 'center',
     overlayX: 'start',
     overlayY: 'center',
-    offsetX: 8
+    offsetX: 8,
+    panelClass: ['right']
   },
   'left': {
     originX : 'start',
     originY : 'center',
     overlayX: 'end',
     overlayY: 'center',
-    offsetX: -8
+    offsetX: -8,
+    panelClass: ['left']
   },
   'top-right': {
     originX: 'end',
     originY: 'top',
     overlayX: 'end',
     overlayY: 'bottom',
-    offsetY: -8
+    offsetY: -8,
+    panelClass: ['top-right']
   },
   'top-left': {
     originX: 'start',
     originY: 'top',
     overlayX: 'start',
     overlayY: 'bottom',
-    offsetY: -8
+    offsetY: -8,
+    panelClass: ['top-left']
   },
   'bottom-right': {
     originX: 'end',
     originY: 'bottom',
     overlayX: 'end',
     overlayY: 'top',
-    offsetY: 8
+    offsetY: 8,
+    panelClass: ['bottom-right']
   },
   'bottom-left': {
     originX: 'start',
     originY: 'bottom',
     overlayX: 'start',
     overlayY: 'top',
-    offsetY: 8
+    offsetY: 8,
+    panelClass: ['bottom-left']
   }
 };
